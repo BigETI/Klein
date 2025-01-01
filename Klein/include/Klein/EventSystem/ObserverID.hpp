efefb6638f1ce50cda456d5dd8fb7e8d465ac8c1
BigETI <bigeti.ethem@gmail.com>
2025-01-01 15:05:56 +0100

    Fixed an issue with exporting observer ID class members.

--- Klein/include/Klein/EventSystem/ObserverID.hpp
+++ Klein/include/Klein/EventSystem/ObserverID.hpp
@@ -3,6 +3,8 @@
 #include <functional>
 #include <utility>
 
+#include "../Exportables/Exportable.hxx"
+
 namespace Klein::EventSystem {
 
 	/// <summary>
@@ -19,70 +21,70 @@ namespace Klein::EventSystem {
 		/// <summary>
 		/// Invalid observer ID
 		/// </summary>
-		static const ObserverID Invalid;
+		KLEIN_API static const ObserverID Invalid;
 
 		/// <summary>
 		/// Constructs a new observer ID
 		/// </summary>
-		ObserverID();
+		KLEIN_API ObserverID();
 
 		/// <summary>
 		/// Constructs a new observer ID and copies the contents of the specified observer ID into this oberver ID
 		/// </summary>
 		/// <param name="observerID">Observer ID to copy from</param>
-		ObserverID(const ObserverID& observerID);
+		KLEIN_API ObserverID(const ObserverID& observerID);
 
 		/// <summary>
 		/// Constructs a new observer ID and moves the contents of the specified observer ID into this oberver ID
 		/// </summary>
 		/// <param name="observerID">Observer ID to move</param>
-		ObserverID(ObserverID&& observerID) noexcept;
+		KLEIN_API ObserverID(ObserverID&& observerID) noexcept;
 
 		/// <summary>
 		/// Creates a new observer ID
 		/// </summary>
 		/// <returns>New observer ID</returns>
-		static ObserverID CreateNew();
+		KLEIN_API static ObserverID CreateNew();
 
 		/// <summary>
 		/// Checks if this observer ID is valid
 		/// </summary>
 		/// <returns>"true" if observer ID is valid, otherwise "false"</returns>
-		bool IsValid() const noexcept;
+		KLEIN_API bool IsValid() const noexcept;
 
 		/// <summary>
 		/// Creates the next observer ID from this observer
 		/// </summary>
 		/// <returns>Next observer ID</returns>
-		ObserverID CreateNextObserverID() const;
+		KLEIN_API ObserverID CreateNextObserverID() const;
 
 		/// <summary>
 		/// Assigns the specified observer ID to this observer ID
 		/// </summary>
 		/// <param name="observerID">Observer ID to assign from</param>
 		/// <returns>Itself</returns>
-		ObserverID& operator =(const ObserverID& observerID);
+		KLEIN_API ObserverID& operator =(const ObserverID& observerID);
 
 		/// <summary>
 		/// Moves the specified observer ID to this observer ID
 		/// </summary>
 		/// <param name="observerID">Observer ID to move</param>
 		/// <returns>Itself</returns>
-		ObserverID& operator =(ObserverID&& observerID) noexcept;
+		KLEIN_API ObserverID& operator =(ObserverID&& observerID) noexcept;
 
 		/// <summary>
 		/// Checks if this observer ID and the specified observer IDs are equal
 		/// </summary>
 		/// <param name="observerID">Observer ID to compare with</param>
 		/// <returns>"true" if both observer IDs are equal, otherwise "false"</returns>
-		bool operator ==(const ObserverID& observerID) const noexcept;
+		KLEIN_API bool operator ==(const ObserverID& observerID) const noexcept;
 
 		/// <summary>
 		/// Checks if this observer ID and the specified observer IDs are not equal
 		/// </summary>
 		/// <param name="observerID">Observer ID to compare with</param>
 		/// <returns>"true" if both observer IDs are not equal, otherwise "false"</returns>
-		bool operator !=(const ObserverID& observerID) const noexcept;
+		KLEIN_API bool operator !=(const ObserverID& observerID) const noexcept;
 
 	private:
 
